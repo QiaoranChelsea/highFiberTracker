6fd6b8d9486cd9df2dd406f49075e2b7b7e7914a
QiaoranChelsea <liqiao@oregonstate.edu>
2019-01-17 20:13:39 -0800

    client(react)+backend(node) set up

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+// client/src/App.js
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>Client Side Is Ready to Use in React :-)</div>;
+  }
+}
+
+export default App;
